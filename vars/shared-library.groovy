ca2e9f040a3b2e74527e8c1663d7f1e0774f2f0d
vaibhaviwarke <vaibhavi.warke79@gmail.com>
2021-10-20 15:28:58 +0530

    update

--- vars/shared-library.groovy
+++ vars/shared-library.groovy
@@ -3,8 +3,10 @@ def CompileCode() {
 }
 
 def BuildDockerImage() {
-    	aws --region ap-south-1 ecr get-login-password | docker login --username AWS --password-stdin 357942556956.dkr.ecr.ap-south-1.amazonaws.com
-    	docker build -t java-project .
+    	sh """
+		aws --region ap-south-1 ecr get-login-password | docker login --username AWS --password-stdin 357942556956.dkr.ecr.ap-south-1.amazonaws.com
+    		docker build -t java-project .
+	"""
 }
 
 def UploadDockerImageToECR(){
