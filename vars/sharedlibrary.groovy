c17ddb768a45eeed97f7d986d0bbaf66dd5a2948
vaibhaviwarke <vaibhavi.warke79@gmail.com>
2021-10-21 16:42:31 +0530

    update

--- vars/sharedlibrary.groovy
+++ vars/sharedlibrary.groovy
@@ -4,7 +4,7 @@ def CompileCode() {
 
 def BuildDockerImage() {
     	sh """
-		$(aws --region ap-south-1 ecr get-login --no-include-email)
+		`aws --region ap-south-1 ecr get-login --no-include-email`
 		#aws --region ap-south-1 ecr get-login-password | docker login --username AWS --password-stdin 357942556956.dkr.ecr.ap-south-1.amazonaws.com
     		docker build -t java-project .
 	"""
@@ -12,12 +12,8 @@ def BuildDockerImage() {
 
 def UploadDockerImageToECR(){
 	sh """
-		echo "Tagging the Docker Image: In Progress"
 		docker tag java-project:latest 357942556956.dkr.ecr.ap-south-1.amazonaws.com/java-project:latest
-		echo "Tagging the Docker Image: Completed"
-		echo "Push Docker Image to ECR : In Progress"
 		docker push 357942556956.dkr.ecr.ap-south-1.amazonaws.com/java-project:latest
-		echo "Push Docker Image to ECR : Completed"
           """
 }
 
